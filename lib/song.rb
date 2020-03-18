a3b807586eec26406c1b1e50ee0aa41403fa533a
German Cruz <cruzgerman216@yahoo.com>
2020-03-18 07:07:40 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -3,6 +3,7 @@ class Song
   attr_accessor :name
   attr_reader :artist
   extend Memorable
+  extend Findable
 
   @@songs = []
 
@@ -10,9 +11,7 @@ class Song
     @@songs << self
   end
 
-  def self.find_by_name(name)
-    @@songs.detect{|a| a.name == name}
-  end
+
 
   def self.all
     @@songs
