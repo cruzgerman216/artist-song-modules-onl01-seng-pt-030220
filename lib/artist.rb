c89bfa8aa20aec3b9a77ba18bb95c038a42b13da
German Cruz <cruzgerman216@yahoo.com>
2020-03-18 05:50:02 +0000

    Automatically backed up by Learn

--- lib/artist.rb
+++ lib/artist.rb
@@ -2,10 +2,7 @@ require 'pry'
 class Artist
   attr_accessor :name
   attr_reader :songs
-  extend Memorable::ClassMethods
-  include Memorable::InstanceMethods
-  extend Findable
-  include Paramable
+  extend Memorable
 
   @@artists = []
 
